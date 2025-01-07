52d9f4810074e8c1167f9e72ce51d54271069689
sherwinromano <sherwin.romano7021@gmail.com>
2025-01-07 15:37:39 +0800

    Added responsiveness

--- src/main.js
+++ src/main.js
@@ -2,25 +2,10 @@ const footerElement = document.querySelector("#footer-fixed");
 const teslaCopyrightYear = document.querySelector(
   ".footer-wrapper-two ul li:nth-child(1)"
 );
-const modal = document.querySelector(".modal");
-const modalOverlay = document.querySelector(".overlay");
-const body = document.querySelector("body");
 
 const currentYear = new Date().getFullYear();
 
 teslaCopyrightYear.innerHTML = `<a href="https://www.tesla.com/about">Tesla &copy; ${currentYear}</a> `;
-
-document.querySelector(".modal-close-button").addEventListener("click", () => {
-  body.classList.replace("popup-active", "popup-inactive");
-  modal.classList.replace("active", "inactive");
-  modalOverlay.classList.replace("active", "inactive");
-});
-
-document.querySelector(".learn-more").addEventListener("click", () => {
-  body.classList.replace("popup-inactive", "popup-active");
-  modal.classList.replace("inactive", "active");
-  modalOverlay.classList.replace("inactive", "active");
-});
 
 window.onload = () => {
   if (window.scrollY >= 200) {
